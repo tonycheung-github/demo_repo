18e84ba4db5b9ea8a0b4234a0d459c53ac4a6e32
Thomas Bayes <tbayes@brainstation.io>
2020-08-06 14:57:09 -0700

    new change to newFeature1 branch

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # demo_repo
 Just a demonstration repository.
-Another Edit
 
 Created By Tony Cheung 
--- my_file.py
+++ my_file.py
@@ -8,3 +8,5 @@ matrix_B = np.random.randn(5,6)
 final_result = np.matmul(matrix_A, matrix_B)
 
 print(final_result.shape)
+
+some stuff in new branch
